4d22ee935eac5dbf27dae1f596ff69d1edfe9fbd
W. Stones <warlockstones@protonmail.com>
2024-09-10 09:23:16 +0200

    Add command buffer to BoundsSystem

--- Assets/Scripts/Systems/BoundsSystem.cs
+++ Assets/Scripts/Systems/BoundsSystem.cs
@@ -1,10 +1,6 @@
-using UnityEngine;
 using Unity.Jobs;
 using Unity.Entities;
 using Unity.Transforms;
-using Unity.Burst;
-using Unity.Mathematics;
-using System.ComponentModel;
 using Unity.Collections;
 
 [RequireMatchingQueriesForUpdate]
@@ -16,15 +12,20 @@ public partial class BoundsSystem : SystemBase
 
     protected override void OnUpdate()
     {
+        var ecb = new EntityCommandBuffer(Allocator.TempJob);
         const float lowestYPos = -5.0f;
         const float highestYPos = 15.0f; // For bullet
+
         Entities.WithAll<LocalToWorld>().ForEach((Entity entity, in LocalToWorld localToWorld) =>
         {
             float yPos = localToWorld.Position.y;
             if (yPos <= lowestYPos || yPos >= highestYPos)
             {
-                EntityManager.DestroyEntity(entity);
+                ecb.DestroyEntity(entity);
             } 
         }).WithStructuralChanges().WithoutBurst().Run(); // Because we are making changes and accessing EntityManager
+
+        ecb.Playback(EntityManager);
+        ecb.Dispose();
     }
 }
